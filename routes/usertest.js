9983c4dc97ef8dbb904115a3a0d01b6c7db938f5
Christophe Gakwaya <c.gakwaya@alustudent.com>
2024-09-22 21:21:39 +0200

    updated

--- routes/usertest.js
+++ routes/usertest.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const router = express.Router();
 const { usertable, accounts } = require('../models'); 
+const { where } = require('sequelize');
 
 // route to create a default user with multiple accounts for testing
 router.get('/test-create-user', async (req, res) => {
@@ -80,5 +81,71 @@ router.post('/add-user-with-accounts', async (req, res) => {
         res.status(500).json({ error: 'Error occurred while creating user with accounts' });
     }
 });
+// updating user info without account
+router.put('/update-user/:id',async(req,res)=>{
+  const{id}=req.params;
+  const {name,email,username,password,country,province}=req.body;
 
+  try{
+    const user = await usertable.findByPk(id);
+    if (!user){
+      return res.status(404).json({message:'user not found'});
+    }
+    await user.update({
+      name,
+      email,
+      username,
+      password,
+      country,
+      province
+    });
+    res.status(200).json({message:'user updated successfully',user});
+
+  } catch(error){
+    console.error('error updating user',error);
+    res.status(500).json({error:'error occured while updating user'});
+  }
+
+});
+router.put('/update-user-with-account/:id',async(req,res)=>{
+  const {id}= req.params;
+  const {name,email,username,password,country,province,accounts}=req.body;
+
+  try{
+    const user = await usertable.findByPk(id,{
+      include:[{
+        model:accounts,
+        as:'accounts'
+      }]
+    });
+    if(!user){
+      return res.status(500).json({message:'user not found'});
+    }
+    await user.update({
+      name,
+      email,
+      username,
+      password,
+      country,
+      province
+    });
+    if (accounts && accounts.length > 0){
+      for(let accountsData of accounts){
+        let account = await user.getAccounts({where:{id: accountData.id}});
+
+        if(account.length>0){
+          await account[0].update(accountData);
+
+        } else {
+          await user.createAccount(accountData);
+        }
+      }
+    }
+    res.status(200).json({message:'user and accounts updated succesfully',user});
+  } catch (error){
+    console.error('error updating user and accounts:',error);
+    res.satus(500).json({error:'error occured while updating  user and accounts'});
+  }
+})
+ 
 module.exports = router;
